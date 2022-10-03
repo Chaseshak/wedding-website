f0fc75a3a3fff42b9089e5dc7d0eccd63738b60f
Chaseshak <chaseshak@gmail.com>
2022-10-02 22:01:09 -0500

    Add faq section

--- js/scripts.js
+++ js/scripts.js
@@ -182,6 +182,26 @@ $(document).ready(function () {
     $('#add-to-cal').html(myCalendar);
 
 
+    /********************** Accordion **********************/
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
